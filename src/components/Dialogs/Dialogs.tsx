8226c464b414744e2a324d9a19ded0bd2bb528a5
Maxim <maksstoma@gmail.com>
2022-02-25 14:50:29 +0300

    (24-25) added arrays and method map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,7 +6,6 @@ type DialogItemType = {
     name: string
     id: number
 }
-
 type MessageType = {
     message: string
 }
@@ -22,25 +21,40 @@ const DialogItem = (props: DialogItemType) => {
 }
 
 const Message = (props: MessageType) => {
+
     return (
         <div className={s.dialog}>{props.message}</div>
     )
 }
 
 const Dialogs = () => {
+    let dialogs = [
+        {id: 1, name: 'Max'},
+        {id: 2, name: 'Bob'},
+        {id: 3, name: 'Rob'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Dima'}
+    ]
+    let messages = [
+        {id: 1, name: 'Hi!'},
+        {id: 2, name: 'How are you?'},
+        {id: 3, name: 'What do you do?'},
+        {id: 4, name: 'Yo'},
+        {id: 5, name: 'Yo'}
+    ]
+
+    let dialogsElements = dialogs.map(d =>
+        <DialogItem name={d.name} id={(d.id)}/>)
+    let messagesElements = messages.map(m =>
+        <Message message={m.name}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Max'} id={1}/>
-                <DialogItem name={'Bob'} id={2}/>
-                <DialogItem name={'Rob'} id={3}/>
-                <DialogItem name={'Sasha'} id={4}/>
-                <DialogItem name={'Dima'} id={5}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={'Hi!'}/>
-                <Message message={'How are you?'}/>
-                <Message message={'What do you do?'}/>
+                {messagesElements}
             </div>
         </div>
     )
