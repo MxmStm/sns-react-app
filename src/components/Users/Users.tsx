d5a71a70538005d430a8bb4d7f50f3e1a2740c43
Maxim <maksstoma@gmail.com>
2022-03-28 23:36:44 +0300

    (50-52) added Axios; Rest API

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {MapPropsUsersType} from "./UsersContainer";
 import s from './Users.module.css'
+import axios from "axios";
 
 export const Users = (props: MapPropsUsersType) => {
 
+    const getUsers = () => {
+        axios.get('https://social-network.samuraijs.com/docs/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
+    }
+
     return (
         <div>
+
+            <button onClick={getUsers}>Get Users</button>
+
             {
                 props.usersPage.users.map(u => <div key={u.id}>
                     <span>
