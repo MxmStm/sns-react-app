8499c23ce2a7f5d6357306713bceb751d380e25a
Maxim <maksstoma@gmail.com>
2022-05-03 12:38:29 +0300

    (75) added redux-form

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,6 +5,7 @@ import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
     usersPage: usersReducer,
     // sidebar: sidebarReducer,
     auth: authReducer,
+    form: formReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
